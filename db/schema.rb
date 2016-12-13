2168d3fb90654a6385ce8bcadca5b7329c3cb73c
Jonathan Serafini <jonathan.serafini@gmail.com>
2016-12-13 23:00:52 +0100

    chartkick

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20161113224815) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
+    t.float    "y"
+    t.float    "x"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
